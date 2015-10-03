01face6c347f9cfff3a16125fce3f2cdabd5dd94
Wellington Dessuy <wellington.dessuy@outlook.com>
2015-10-02 23:58:25 -0300

    Implementado a copia do arquivo de configuração do nginx no bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,17 @@
 sudo -i
 
 apt-get update
+apt-get upgrade
 
 # Install Nginx
 apt-get -y install nginx
 
 # Install PHP
 apt-get -y install php5 libapache2-mod-php5 php5-mcrypt php5-cli php5-fpm
+
+# Removendo o arquivo de configuração padrão do Nginx
+rm /etc/nginx/nginx.conf
+# Link do arquivo de configuração existente no projeto
+ln -s /vagrant/config/nginx.conf /etc/nginx/
+# Reiniciando o Nginx
+service nginx restart
