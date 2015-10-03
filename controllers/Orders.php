27aa2e0c1d9ff8c872bb60cbeb60b74b08df6ff1
Wellington Dessuy <wellington.dessuy@outlook.com>
2015-10-03 12:55:25 -0300

    Slim

--- controllers/Orders.php
+++ controllers/Orders.php
@@ -1,24 +1,42 @@
-<?php 
+<?php
 
 class Orders {
 
+	private $request;
+	private $response;
+
+	public function __construct(&$request, &$response) {
+		$this->request = $request;
+		$this->response = $response;
+	}
+
 	public function insert() {
+		echo $this->request->getMethod();
+		echo '   ';
 		echo 'insert';
 	}
 
 	public function findById($id) {
+		echo $this->request->getMethod();
+		echo '   ';
 		echo 'findById' . $id;
 	}
 
 	public function findAll() {
+		echo $this->request->getMethod();
+		echo '   ';
 		echo 'findAll';
 	}
 
 	public function update($id) {
+		echo $this->request->getMethod();
+		echo '   ';
 		echo 'update' . $id;
 	}
 
 	public function delete($id) {
+		echo $this->request->getMethod();
+		echo '   ';	
 		echo 'delete' . $id;
 	}
 
--- index.php
+++ index.php
@@ -3,38 +3,41 @@
 require 'vendor/autoload.php';
 require_once('controllers/Orders.php');
 
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 $app = new \Slim\Slim();
 
 // API group
 $app->group('/api', function () use ($app) {
  	
-    $app->post('/:controller', function ($controller) {
+    $app->post('/:controller', function ($controller) use ($app) {
     	$controller = ucfirst($controller);
-    	$controller = new $controller();
+    	$controller = new $controller($app->request, $app->response);
     	$controller->insert();
     });
 
-	$app->get('/:controller', function ($controller) {
+	$app->get('/:controller', function ($controller) use ($app) {
     	$controller = ucfirst($controller);
-    	$controller = new $controller();
+    	$controller = new $controller($app->request, $app->response);
     	$controller->findAll();
     });    
 
-    $app->get('/:controller/:id', function ($controller, $id) {
+    $app->get('/:controller/:id', function ($controller, $id) use ($app) {
     	$controller = ucfirst($controller);
-    	$controller = new $controller();
+    	$controller = new $controller($app->request, $app->response);
     	$controller->findById($id);
     });
 
-    $app->put('/:controller/:id', function ($controller, $id) {
+    $app->put('/:controller/:id', function ($controller, $id) use ($app) {
     	$controller = ucfirst($controller);
-    	$controller = new $controller();
+    	$controller = new $controller($app->request, $app->response);
     	$controller->update($id);
     });
 
-    $app->delete('/:controller/:id', function ($controller, $id) {
+    $app->delete('/:controller/:id', function ($controller, $id) use ($app) {
     	$controller = ucfirst($controller);
-    	$controller = new $controller();
+    	$controller = new $controller($app->request, $app->response);
     	$controller->delete($id);
     });	    
 
